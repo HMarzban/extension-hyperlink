004ca11305a62ab5af20b497198bca97a003c830
hossein <marzban98@gmail.com>
2023-07-19 15:04:50 +0330

    Remove dist folder from repository

--- packages/extension-hyperlink/dist/helpers/autoHyperlink.d.ts
+++
@@ -1,8 +0,0 @@
-import { MarkType } from "@tiptap/pm/model";
-import { Plugin } from "@tiptap/pm/state";
-type AutoHyperlinkOptions = {
-    type: MarkType;
-    validate?: (url: string) => boolean;
-};
-export default function autoHyperlink(options: AutoHyperlinkOptions): Plugin;
-export {};
--- packages/extension-hyperlink/dist/helpers/editHyperlink.d.ts
+++
@@ -1,9 +0,0 @@
-import { Editor } from "@tiptap/core";
-type EditHyperlinkOptions = {
-    editor: Editor;
-    validate?: (url: string) => boolean;
-    newURL?: string;
-    newText?: string;
-};
-export default function editHyperlink(options: EditHyperlinkOptions): boolean;
-export {};
